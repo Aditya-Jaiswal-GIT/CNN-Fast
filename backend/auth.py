06932f30ed6a6cc2fb8fb0ed44324f66ca5227f4
Aditya Jaiswal <adibhai212005@gmail.com>
2026-05-21 20:18:54 +0530

    Add Authentication

---
+++ backend/auth.py
@@ -0,0 +1,85 @@
+import jwt
+from fastapi import HTTPException,Depends
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+import os
+import json
+from dotenv import load_dotenv
+load_dotenv()
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+def load():
+    with open(rf"data.json",'r') as f :
+        data = json.load(f)
+    return data
+def store(data:dict):
+    with open(rf"data.json",'w') as f :
+        store = json.dump(data,f,indent=4)
+jwt_secret = os.getenv('JWT_TOKEN')
+jwt_algo = "HS256"
+oauth = OAuth2PasswordBearer(tokenUrl="signin")
+
+def verify(plainpassword:str,hashpassword:str):
+    return pwd_context.verify(plainpassword,hashpassword)
+
+def hash(password:str):
+    hashed_pwd = pwd_context.hash(password)
+    return hashed_pwd
+
+def create(data:dict):
+    jsons = load()
+    for json in jsons:
+        if data["email"]==json["email"]:
+            raise HTTPException(status_code=400,detail="Email already exist")
+        if data["username"]==json["username"]:
+            raise HTTPException(status_code=400,detail="Username already registerd")
+    hash_pwd = hash(data["password"])
+    user = {"email" : data["email"],"username":data["username"],"password":hash_pwd}
+    print(data)
+    print(type(data["password"]))
+    print(len(data["password"]))
+    jsons.append(user)
+    store(jsons)
+
+def authenticate(data:dict):
+    jsons = load()
+    flag=0
+    for json in jsons:
+        if json["username"]==data["username"]:
+            flag=1
+            v = verify(data["password"],json["password"])
+            if not v:
+                raise HTTPException(status_code=400,detail="Incorrect password")
+            user = json
+    if flag==0:
+        raise HTTPException(status_code=400,detail="Username Does not exist")
+    else :
+        return user
+
+
+def encode(data:dict):
+    payload = data.copy()
+    token = jwt.encode(payload,jwt_secret, algorithm=jwt_algo)
+    return token
+def decode(token):
+    try:
+        decoded = jwt.decode(token,jwt_secret,algorithms=[jwt_algo])
+        return decoded
+    except jwt.ExpiredSignatureError:
+        return {'error' : "Token Expired"}
+    except jwt.InvalidTokenError:
+        return {'error' : "Invalid Token"}
+async def get_current_user(
+    token: str = Depends(oauth)
+):
+    payload = decode(token)
+
+    if "error" in payload:
+        raise HTTPException(
+            status_code=401,
+            detail=payload["error"]
+        )
+
+    return payload
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, HTTPException, File, UploadFile,Request
+from fastapi import FastAPI, HTTPException, File, UploadFile,Request,Depends
 from fastapi.responses import JSONResponse
 from model import prediction
-
+from auth import create,authenticate,encode,decode,get_current_user,hash
+from schema import Signup,Signin
 from io import BytesIO
 from PIL import Image
 from slowapi import Limiter
@@ -10,6 +11,7 @@ from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
 app = FastAPI()
 limiter = Limiter(key_func=get_remote_address)
+
 
 # add limiter to app
 app.state.limiter = limiter
@@ -27,6 +29,26 @@ async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
         }
     )
 
+
+@app.post("/signup")
+@limiter.limit("5/minute")
+def signup(data:Signup,request:Request):
+    create(data.model_dump())
+    return JSONResponse(status_code=200,content="User created Sucessfully")
+
+@app.post("/signin")
+@limiter.limit("5/minute")
+def signin(data:Signin,request:Request):
+   
+    user =authenticate(data.model_dump())
+    token = encode({
+        "username" : user["username"]
+    })
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
+
 @app.get('/')
 @limiter.limit('5/minute')
 async def home(request:Request):
@@ -39,7 +61,7 @@ async def health(request:Request):
     }
 @app.post('/predict')
 @limiter.limit('5/minute')
-async def predict(request:Request,file: UploadFile = File(...)):
+async def predict(request:Request,file: UploadFile = File(...),current=Depends(get_current_user)):
     try:
         contents = await file.read()
         img = Image.open(BytesIO(contents))

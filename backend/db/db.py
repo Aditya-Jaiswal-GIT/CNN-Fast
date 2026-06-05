ef754f9306d2a6c8865415e364307adcdaf317c8
Aditya Jaiswal <adibhai212005@gmail.com>
2026-06-05 16:50:07 +0530

    Remove Index

--- backend/db/db.py
+++ backend/db/db.py
@@ -6,12 +6,3 @@ client = MongoClient(os.getenv("DB_URL"))
 db = client["Lung_DB"]
 users_collection = db["user"]
 # Unique indexes
-users_collection.create_index(
-    "email",
-    unique=True
-)
-
-users_collection.create_index(
-    "username",
-    unique=True
-)
